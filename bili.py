df6104cf5f8a1611bea2fcfe1b7c5d48283a04d9
liziwl <leezisy@gmail.com>
2017-11-27 21:34:51 +0800

    finish basic function

--- bili.py
+++ bili.py
@@ -1,4 +1,3 @@
-import codecs
 import random
 import threading
 from functools import namedtuple
@@ -9,12 +8,17 @@ import requests
 from lxml import etree
 import re
 
-# TODO 速度还是快了，会被封ip
+# 不要轻易改小sleep数值，否则会被封IP。
+# 有些视频需要登陆才能查看，所以在basic_info里播放量会为"--"。
 
-basic_header = ["aid", "view", "danmaku", "reply", "favorite", "coin", "share"]
+basic_header = ["aid", "view", "danmaku", "reply", "favorite", "coin", "share","tag_attention"]
 detail_header = ["aid", "title", "category", "date", "duration", "upid"]
+up_header = ["mid", "name", "fans", "video"]
+
 Video = namedtuple('Video', basic_header)
 Video_detail = namedtuple('Video_detail', detail_header)
+Up_detail = namedtuple('up_detail', up_header)
+
 headers = {
     'X-Requested-With': 'XMLHttpRequest',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
@@ -22,21 +26,34 @@ headers = {
 }
 total = 1
 detail_total = 1
+up_total = 1
+
 result = []
 detail_result = []
+up_result = []
+
 lock = threading.Lock()
+
 
 def basic_info(url):
     """
     抓取视频编号，播放量，弹幕数，评论数，收藏数，硬币数，分享数
     :param url: 网址
-    :return:
     """
     global total
-    ti.sleep(0.5)  # 延迟，避免太快 ip 被封
+    ti.sleep(1)  # 延迟，避免太快 ip 被封
     req = requests.get(url, headers=headers, timeout=6).json()
+    ti.sleep(1)  # 延迟，避免太快 ip 被封
+    av = url.replace("http://api.bilibili.com/archive_stat/stat?aid=", "")
+    tag_url = "https://api.bilibili.com/x/tag/archive/tags?aid="+str(av)
+    tag_req = requests.get(tag_url, headers=headers, timeout=6).json()
     try:
         data = req['data']
+        total_tag_atten = 0
+        tag_data = tag_req['data']
+        for i in range(0,len(tag_data)):
+            total_tag_atten+=tag_data[i]['count']['atten']
+
         video = Video(
             data['aid'],  # 视频编号
             data['view'],  # 播放量
@@ -44,8 +61,10 @@ def basic_info(url):
             data['reply'],  # 评论数
             data['favorite'],  # 收藏数
             data['coin'],  # 硬币数
-            data['share']  # 分享数
+            data['share'],  # 分享数
+            total_tag_atten # 标签总关注数
         )
+
         with lock:
             result.append(video)
             print(total)
@@ -54,14 +73,42 @@ def basic_info(url):
         pass
 
 
+def up_info(url):
+    """
+    抓取up名称，粉丝数，投稿数
+    :param url: 网址
+    """
+    global up_total
+    ti.sleep(1)  # 延迟，避免太快 ip 被封
+    req = requests.get(url, headers=headers, timeout=6).json()
+    try:
+        data = req['data']
+        card = data['card']
+        mid = card['mid']
+        name = card['name']
+        fans = card['fans']
+        url_de = "https://api.bilibili.com/x/space/navnum?mid=" + str(mid)
+        ti.sleep(1)  # 延迟，避免太快 ip 被封
+        req_de = requests.get(url_de, headers=headers, timeout=6).json()
+        data = req_de['data']
+        video = data['video']
+
+        tmp = Up_detail(mid, name, fans, video)
+        with lock:
+            up_result.append(tmp)
+            print(up_total)
+            up_total += 1
+    except:
+        pass
+
+
 def detailed_info(url):
     """
     抓取视频标题，上传时间，视频类型，视频标签，时长
     :param url: 网址
-    :return:
     """
     global detail_total
-    ti.sleep(0.5)
+    ti.sleep(1)
     html = requests.get(url, headers=headers, timeout=6)
     selector = etree.HTML(html.text)
     content = selector.xpath("//html")
@@ -107,8 +154,8 @@ def detailed_info(url):
                 cids = re.findall(r'cid=.+&aid', cid_html)
                 cid = cids[0].replace("cid=", "").replace("&aid", "")
                 info_url = "http://interface.bilibili.com/player?id=cid:" + str(cid) + "&aid=" + av
-                ti.sleep(0.5)
-                video_info = requests.get(info_url)
+                ti.sleep(1)
+                video_info = requests.get(info_url, headers=headers, timeout=6)
                 video_selector = etree.HTML(video_info.text)
                 for video_each in video_selector:
                     duration_log = video_each.xpath('//duration/text()')
@@ -150,12 +197,50 @@ def generate_url(to_num, size):
     return out, out_detail
 
 
+def get_mid(file):
+    mid = []
+    with open(file, encoding="utf_8_sig") as f:
+        f_csv = csv.reader(f)
+        headers = next(f_csv)
+        for row in f_csv:
+            mid.append(row[5])
+            # print(row[5])
+    return mid
+
+
+def generate_mid_url(mids):
+    out = []
+    for i in mids:
+        url = "https://api.bilibili.com/cardrich?mid={}".format(i)
+        out.append(url)
+    return out
+
+
 if __name__ == "__main__":
-    urls, detail_url = generate_url(17000000, 3000)
-    with futures.ThreadPoolExecutor(32) as executor:
-        executor.map(basic_info, urls)
-        executor.map(detailed_info, detail_url)
-    print(result)
-    save("basic_info", basic_header, result)
-    print(detail_result)
-    save("detail_info", detail_header, detail_result)
+    pass
+    # ------------------------------------------------
+    # 第一次运行这一部分，随机采样数据，生成2个csv文件。
+    # ------------------------------------------------
+
+    # urls, detail_url = generate_url(17000000, 4000)
+    # with futures.ThreadPoolExecutor(32) as executor:
+    #     executor.map(basic_info, urls)
+    # print(result)
+    # save("basic_info", basic_header, result)
+    #
+    # with futures.ThreadPoolExecutor(32) as executor:
+    #     executor.map(detailed_info, detail_url)
+    # print(detail_result)
+    # save("detail_info", detail_header, detail_result)
+
+    # -----------------------------------------------------
+    # 第二次运行这一部分，根据上面采样出的视频找up的数据
+    # ------------------------------------------------
+
+    # mid = get_mid("detail_info.csv")
+    # mid_url = generate_mid_url(mid)
+
+    # with futures.ThreadPoolExecutor(32) as executor:
+    #     executor.map(up_info, mid_url)
+    # print(up_result)
+    # save("up_info", up_header, up_result)
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
-## bilibili数据分析爬虫
+# B站爬虫
+## 本次bilibili数据分析所需要的数据
 ### 因变量（Y）
 - [x] b站视频播放量
 ### 自变量（X）
@@ -6,14 +7,22 @@
 - [x] 视频号码
 - [x] 上传时间
 - [x] 视频类型
-- [ ] 视频标签
+- [x] 视频标签
 - [x] 视频获得的硬币量
 - [x] 视频获得的收藏次数
 - [x] 视频获得的分享次数
 - [x] 视频时长
-- [ ] up主（视频上传者）投稿数量
-- [ ] up主（视频上传者）粉丝数。
+- [x] up主（视频上传者）投稿数量
+- [x] up主（视频上传者）粉丝数。
 
+## 运行环境
+    * Python3
+    * PyCharm
+    
+### 第三方包
+    * lxml
+    
+## 调用的API
 1. 基本信息
 
     ```http://api.bilibili.com/archive_stat/stat?aid={}```
@@ -27,12 +36,22 @@
 
 2. tag信息
 
-    ```https://api.bilibili.com/x/tag/archive/tags?aid={}```
+    ```http://api.bilibili.com/x/tag/archive/tags?aid={}```
     * 获取aid所对应的所有标签
 
 3. 直接访问页面
+
+    ```http://bilibili.com/video/av{}```
     * 视频名称
     * 上传时间
     * 视频类型
     * 视频标签
     * 视频时长
+    
+4. 获取UP信息
+
+    1. ```http://api.bilibili.com/cardrich?mid=777536```
+        * up名
+        * 粉丝数
+    2. ```http://api.bilibili.com/x/space/navnum?mid=1351379```
+        * 上传数
